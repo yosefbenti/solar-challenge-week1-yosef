34b17634e8d1ecf26e4068a4e00622050851963e
yosefbenti <83132056+yosefbenti@users.noreply.github.com>
2025-05-20 11:59:48 +0300

    Update main.py
    updated

--- app/main.py
+++ app/main.py
@@ -6,25 +6,30 @@ import matplotlib.pyplot as plt
 # This must be the very first Streamlit command!
 st.set_page_config(page_title="Solar Dashboard", layout="centered")
 
-st.sidebar.header("Upload CSV Files")
+def load_uploaded_data():
+    st.sidebar.header("Upload CSV Files")
+    benin_file = st.sidebar.file_uploader("Upload benin_clean.csv", type=["csv"])
+    sierra_file = st.sidebar.file_uploader("Upload sierraleone_clean.csv", type=["csv"])
+    togo_file = st.sidebar.file_uploader("Upload togo_clean.csv", type=["csv"])
 
-benin_file = st.sidebar.file_uploader("Upload benin_clean.csv", type=["csv"])
-sierra_leone_file = st.sidebar.file_uploader("Upload sierra_leone_clean.csv", type=["csv"])
-togo_file = st.sidebar.file_uploader("Upload togo_clean.csv", type=["csv"])
+    if benin_file and sierra_file and togo_file:
+        df1 = pd.read_csv(benin_file)
+        df2 = pd.read_csv(sierra_file)
+        df3 = pd.read_csv(togo_file)
+        df1['Country'] = 'Benin'
+        df2['Country'] = 'Sierra_Leone'
+        df3['Country'] = 'Togo'
+        return pd.concat([df1, df2, df3], ignore_index=True)
+    else:
+        st.warning("Please upload all three CSV files from the sidebar to proceed.")
+        return None
 
-@st.cache_data
-def load_data(benin_file, sierra_leone_file, togo_file):
-    df1 = pd.read_csv(benin_file)
-    df2 = pd.read_csv(sierra_leone_file)
-    df3 = pd.read_csv(togo_file)
-    df1['Country'] = 'Benin'
-    df2['Country'] = 'Sierra_Leone'
-    df3['Country'] = 'Togo'
-    return pd.concat([df1, df2, df3], ignore_index=True)
+# Comment out or remove the old load_data() usage
+# df = load_data()
 
-if benin_file and sierra_leone_file and togo_file:
-    df = load_data(benin_file, sierra_leone_file, togo_file)
+df = load_uploaded_data()
 
+if df is not None:
     st.title("Cross-Country Solar Energy Dashboard")
 
     countries = st.multiselect("Select Countries", df['Country'].unique(), default=list(df['Country'].unique()))
@@ -42,5 +47,3 @@ if benin_file and sierra_leone_file and togo_file:
     st.subheader("Top Regions by Average GHI")
     top = df.groupby('Country')['GHI'].mean().sort_values(ascending=False).reset_index()
     st.dataframe(top)
-else:
-    st.warning("Please upload all three CSV files to proceed.")
